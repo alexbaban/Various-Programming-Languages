77d3a9697ca5f2782218236a9bc80eb61eb043d1
Alex Baban <alexbaban@users.noreply.github.com>
2019-03-16 22:19:13 -0700

    Create KT0913_AM.ino

---
+++ Arduino/KT0913_AM.ino
@@ -0,0 +1,75 @@
+// KT0913 (AM Radio)
+
+#include <Wire.h>
+
+int channel = 0;
+
+// AM frequency in KHz
+double frequency = 690;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+   // channel index formula
+   channel = frequency / 1;
+
+   Wire.begin();
+
+     
+   //// AM Mode Control
+   Wire.beginTransmission(0x35);
+
+   // register
+   Wire.write(0x16);
+       
+   // data
+   Wire.write(0b10000000);   
+     
+   // data
+   Wire.write(0b00000010); 
+      
+   Wire.endTransmission();   
+   delay(100); 
+   
+
+   //// AM Tune Enable, AM Channel Setting
+   Wire.beginTransmission(0x35);
+
+   // register
+   Wire.write(0x17);
+       
+   // data
+   Wire.write(0b10000000 | (channel >> 8));
+      
+   // data
+   Wire.write(0b11111111 & channel);
+      
+   Wire.endTransmission();   
+   delay(100);
+
+
+   //// Standby Mode Enable, Volume Control
+   Wire.beginTransmission(0x35);
+
+   // register address
+   Wire.write(0x0F);
+       
+   // data
+   Wire.write(0b10001000);   
+     
+   // data
+   Wire.write(0b00011111); 
+      
+   Wire.endTransmission();   
+   delay(100);   
+  
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
