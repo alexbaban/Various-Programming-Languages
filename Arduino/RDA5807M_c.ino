11dbccfe5cb89a9c62b5dde373ec6827ef7f38d7
Alex Baban <alexbaban@users.noreply.github.com>
2019-03-29 10:06:41 -0700

    Update RDA5807M_c.ino

--- Arduino/RDA5807M_c.ino
+++ Arduino/RDA5807M_c.ino
@@ -8,9 +8,12 @@ const double frequency = 105.7;
 
 void setup() {
 
+  Serial.begin(9600);
+  Serial.println("RDA5807M FM Tuner Example\n");
+
   Wire.begin();
   delay(500);
-  
+
   // formula to calculate decimal value of PLL word
   // high side injection
   int frequencyB = 4 * (frequency * 1000000 + 225000) / 32768;
@@ -20,7 +23,7 @@ void setup() {
   // chip
   Wire.beginTransmission(0x60);
 
-  // data sequence: 
+  // write data sequence: 
   // byte 1, byte 2, byte 3, byte 4 and byte 5 
   Wire.write(frequencyH & 0b00111111);
   Wire.write(frequencyL);
@@ -29,10 +32,52 @@ void setup() {
   Wire.write(0b01000000);
 
   Wire.endTransmission();
+
+  delay(1000);
   
 }
 
 void loop() {
-  // nothing
+
+  // unsigned char frequencyH = 0;
+  // unsigned char frequencyL = 0;
+  double freqHz = 0; 
+
+  unsigned char aBuffer[5];
+  memset (aBuffer, 0, 5);
+  
+  // read
+  Wire.requestFrom(0x60, 5);
+  
+  if (Wire.available()) {
+    
+    for (int i=0; i<5; i++) {
+      aBuffer[i]= Wire.read();
+    }
+
+    freqHz = (((aBuffer[0] & 0b00111111) << 8) + aBuffer[1]) * 32768 / 4 - 225000;
+
+    Serial.print("\nFM: ");
+    Serial.print((freqHz / 1000000));
+    Serial.println(" MHz");
+
+    // frequencyH = ((aBuffer[0] & 0b00111111));
+    // frequencyL = aBuffer[1];
+   
+    if (aBuffer[2] & 0b10000000) {
+      Serial.println("STEREO");
+    } else {
+      Serial.println("MONO");
+    }
+
+    Serial.print("Level: ");
+    Serial.println((aBuffer[3] & 0b11110000) >> 4, DEC);
+
+    // search mode
+    // (aBuffer[0] & 0b10000000)
+
+  }
+
+  delay(5000);
   
 }
